f50a16a3be96b88d3278d0974c271de92914582f
Alexander Vanlancker <alexander.vanlancker@codifly.be>
2022-09-28 13:56:50 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <NavBar />
+    <div id="app">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <EmployeeTable msg="Welcome to Your Vue.js App"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import EmployeeTable from './components/EmployeeTable.vue'
+import NavBar from './components/NavBar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    EmployeeTable,
+    NavBar
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0 100px;
+  margin-top: 60px;
+}
+</style>
+
+<style scoped>
+  img {
+    max-width: 100px;
+    margin-bottom: 2rem;
+  }
+</style>
---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <b-table hover :items="employees" :fields="fields" striped responsive="sm" :tbody-tr-class="rowClass">
+      <template #cell(show_details)="row">
+        <b-button size="sm" @click="row.toggleDetails" class="mr-2">
+          {{ row.detailsShowing ? 'Hide' : 'Show' }} Details
+        </b-button>
+      </template>
+      <template #row-details="row">
+        <b-card>
+          <b-row class="mb-2">
+            <b-col sm="3" class="text-sm-right"><b>employee number:</b></b-col>
+            <b-col>{{ row.item.employeeId }}</b-col>
+            <b-col sm="3" class="text-sm-right"><b>phoneNumber:</b></b-col>
+            <b-col>{{ row.item.phoneNumber }}</b-col>
+          </b-row>
+          <b-row class="mb-2">
+            <b-col sm="3" class="text-sm-right"><b>hireDate:</b></b-col>
+            <b-col>{{ row.item.hireDate }}</b-col>
+            <b-col sm="3" class="text-sm-right"><b>salary:</b></b-col>
+            <b-col>{{ row.item.salary }}</b-col>
+          </b-row>
+          <b-row class="mb-2">
+            <b-col sm="3" class="text-sm-right"><b>jobId:</b></b-col>
+            <b-col>{{ row.item.jobId }}</b-col>
+            <b-col></b-col>
+            <b-col></b-col>
+          </b-row>
+          <b-row class="mb-2">
+            <b-col sm="3" class="text-sm-right"><b>manager:</b></b-col>
+            <b-col>{{
+              row.item.manager
+                ? `${row.item.manager.firstName} ${row.item.manager.lastName}`
+                : ''
+            }}</b-col>
+            <b-col></b-col>
+            <b-col></b-col>
+          </b-row>
+
+          <b-button size="sm" @click="row.toggleDetails">Hide Details</b-button>
+        </b-card>
+      </template>
+    </b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmployeeTable',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      fields: [
+        'firstName',
+        'lastName',
+        'show_details',
+      ],
+      employees: [],
+      highlightManagers: false,
+    };
+  },
+  methods: {
+    rowClass(item, type) {
+      if (!item || type !== 'row') return;
+      if (item.inverseManager?.length > 0 && this.highlightManagers) return 'table-success';
+    },
+  },
+  mounted() {
+    fetch('https://localhost:7294/api/Employee')
+      .then((res) => res.json())
+      .then((data) => (this.employees = data));
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
